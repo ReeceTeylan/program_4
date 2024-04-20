eced1cb59b82df70cbedd1faf0d785cb445bae89
ReeceTeylan <rixteylan24@gmail.com>
2024-04-20 20:34:24 +0800

    added pseudocode

--- program_4_2.py
+++ program_4_2.py
@@ -1,1 +1,6 @@
-#"output  the student with highest GWA from reading a file"git stat
+#"output  the student with highest GWA from reading a file"
+
+# read the file with student names and GWA
+# find the student with highest GWA
+# output the name of the student with the highest GWA
+# add design to output
