3250812b4f21708a6ada8e295a1402f6421ba8a9
ReeceTeylan <rixteylan24@gmail.com>
2024-04-20 19:40:05 +0800

    add pseudocode

--- program_4_1.py
+++ program_4_1.py
@@ -1,1 +1,8 @@
 #"split integers into 2 files, odd or even"
+
+# open numbers.txt file
+# read the 20 integers
+# open even.txt file
+# write even numbers to the file
+# open odd.txt file
+# write odd numbers to the file
