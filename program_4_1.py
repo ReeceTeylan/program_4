d8a3d84595e221fe12f89b910e2e7337ca6cf8cf
ReeceTeylan <rixteylan24@gmail.com>
2024-04-20 20:15:40 +0800

    open even.txt file

--- program_4_1.py
+++ program_4_1.py
@@ -5,6 +5,7 @@ with open("numbers.txt", "r") as number_file:
 # read the 20 integers
     numbers = [int(line.strip()) for line in number_file.readlines()[:20]]
 # open even.txt file
+with open('even.txt', 'w') as even_file:
 # write even numbers to the file
 # open odd.txt file
 # write odd numbers to the file
