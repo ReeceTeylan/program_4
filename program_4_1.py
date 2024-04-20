24f9f49cf61ca46b05993ab5f95fe236088b30fd
ReeceTeylan <rixteylan24@gmail.com>
2024-04-20 20:12:14 +0800

    read the 20 integers

--- program_4_1.py
+++ program_4_1.py
@@ -1,8 +1,9 @@
 #"split integers into 2 files, odd or even"
 
 # open numbers.txt file
-with open("numbers.txt", "r") as number_file
+with open("numbers.txt", "r") as number_file:
 # read the 20 integers
+    numbers = [int(line.strip()) for line in number_file.readlines()[:20]]
 # open even.txt file
 # write even numbers to the file
 # open odd.txt file
