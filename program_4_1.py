37029abfff3bebe7dbc7725f0c9d44453ea172d3
ReeceTeylan <rixteylan24@gmail.com>
2024-04-20 19:42:51 +0800

    open numbers.txt file

--- program_4_1.py
+++ program_4_1.py
@@ -1,6 +1,7 @@
 #"split integers into 2 files, odd or even"
 
 # open numbers.txt file
+with open("numbers.txt", "r") as number_file
 # read the 20 integers
 # open even.txt file
 # write even numbers to the file
