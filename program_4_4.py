9f621c73a8a645d202ba8b773ec390d31aff4937
ReeceTeylan <rixteylan24@gmail.com>
2024-04-23 20:26:10 +0800

    Added pseudocode

--- program_4_4.py
+++ program_4_4.py
@@ -1,1 +1,6 @@
 #"create 2 seperate files with squares of even integers and cube of odd integers from a source text file"
+
+# open the file contatining 20 integers
+# identify the even integers
+# square all the even integers and place in double.txt
+# cube all the odd integers and place in triple.txt
