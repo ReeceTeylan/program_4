6d67fca56e0ec8a40195adc9abe76113083fe887
ReeceTeylan <rixteylan24@gmail.com>
2024-04-23 18:58:45 +0800

    read the file with student names and GWA

--- program_4_2.py
+++ program_4_2.py
@@ -1,6 +1,13 @@
 #"output  the student with highest GWA from reading a file"
 
 # read the file with student names and GWA
+def find_highest():
+    with open("students.txt", "r") as student_file:
+        info = student_file.read()
+        info_list = eval(info)
+        highest_gwa = None
+        highest_gwa_student = None
 # find the student with highest GWA
 # output the name of the student with the highest GWA
 # add design to output
+
