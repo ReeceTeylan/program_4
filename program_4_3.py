451e304b5f16d387d154ed0f3db154d75c09abef
ReeceTeylan <rixteylan24@gmail.com>
2024-04-23 19:55:47 +0800

    add input for the lines

--- program_4_3.py
+++ program_4_3.py
@@ -1,5 +1,10 @@
 #"write multiple line of contents into a text file"
 
 # add input for the lines
+proceed = True
+while proceed:
+    line = input("Enter line: ")
+    break_line = line+("\n")
+# open mylife.txt to store the lines
 # add a loop for more lines
 # finish the code
