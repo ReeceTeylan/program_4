da76a90bf3887a587c1fb56b57dbd39c692a2263
ReeceTeylan <rixteylan24@gmail.com>
2024-04-23 20:27:48 +0800

    open the file contatining 20 integers

--- program_4_4.py
+++ program_4_4.py
@@ -1,6 +1,7 @@
 #"create 2 seperate files with squares of even integers and cube of odd integers from a source text file"
 
 # open the file contatining 20 integers
+with open("integers.txt", "r") as integer_file:
 # identify the even integers
 # square all the even integers and place in double.txt
 # cube all the odd integers and place in triple.txt
