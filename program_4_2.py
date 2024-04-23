2d783b76de540f37e953d3dbe9a4c9ed964cde58
ReeceTeylan <rixteylan24@gmail.com>
2024-04-23 19:04:16 +0800

    find the student with highest GWA

--- program_4_2.py
+++ program_4_2.py
@@ -8,6 +8,13 @@ def find_highest():
         highest_gwa = None
         highest_gwa_student = None
 # find the student with highest GWA
+        for name, grade in info_list.items():
+            float_value = float(grade)
+            if highest_gwa is None or float_value < float(highest_gwa):
+                highest_gwa_student = name
+                highest_gwa = grade
+        return highest_gwa_student, highest_gwa
+name, grade = find_highest()
 # output the name of the student with the highest GWA
 # add design to output
 
