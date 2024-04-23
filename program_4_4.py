4109ee227b9db208e318aef51d03feae1360da62
ReeceTeylan <rixteylan24@gmail.com>
2024-04-23 20:31:15 +0800

    identify the even integers

--- program_4_4.py
+++ program_4_4.py
@@ -3,5 +3,8 @@
 # open the file contatining 20 integers
 with open("integers.txt", "r") as integer_file:
 # identify the even integers
+    for line in integer_file:
+        line = int(line)
+        if line % 2 == 0:
 # square all the even integers and place in double.txt
 # cube all the odd integers and place in triple.txt
